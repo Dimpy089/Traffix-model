185a3ccb6819080b17a9b016c68ce33db77c9559
Dimpy089 <dimpyrewari06@gmail.com>
2026-05-18 13:55:54 +0530

    added controllers

---
+++ backend/controllers/authcontrollers.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({ name, email, password: hashedPassword });
+        await newUser.save();
+
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+
+        res.status(201).json({ token });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.login
